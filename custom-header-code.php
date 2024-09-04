90f5ef66a18ef26840ca54fa89d067fa4f1af15c
kwtki <156676691+kwtki@users.noreply.github.com>
2024-09-04 17:34:27 +0900

    first commit

---
+++ custom-header-code.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Plugin Name: Custom Header Code
+ * Description: A simple plugin to add custom code to the header.
+ * Version: 1.0
+ * Author: Your Name
+ */
+
+// Prevent direct access to the file
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+// Function to add custom code to the header
+function chc_add_custom_code_to_header() {
+    $custom_code = get_option('chc_custom_header_code', '');
+    if (!empty($custom_code)) {
+        echo $custom_code;
+    }
+}
+add_action('wp_head', 'chc_add_custom_code_to_header');
+
+// Create admin menu item under Settings > Head Tag Editor
+function chc_create_admin_menu() {
+    add_options_page(
+        'Head Tag Editor',         // Page title
+        'Head Tag Editor',         // Menu title
+        'manage_options',          // Capability
+        'custom-header-code',      // Menu slug
+        'chc_admin_page_callback'  // Callback function
+    );
+}
+add_action('admin_menu', 'chc_create_admin_menu');
+
+// Admin page callback function
+function chc_admin_page_callback() {
+    ?>
+    <div class="wrap">
+        <h1>Head Tag Editor</h1>
+        <form method="post" action="options.php">
+            <?php
+                settings_fields('chc-settings-group');
+                do_settings_sections('chc-settings-group');
+                $custom_code = get_option('chc_custom_header_code', '');
+            ?>
+            <textarea name="chc_custom_header_code" rows="10" cols="50" class="large-text"><?php echo esc_textarea($custom_code); ?></textarea>
+            <?php submit_button(); ?>
+        </form>
+    </div>
+    <?php
+}
+
+// Register the setting
+function chc_register_settings() {
+    register_setting('chc-settings-group', 'chc_custom_header_code');
+}
+add_action('admin_init', 'chc_register_settings');
---
+++ readme.md
@@ -0,0 +1,15 @@
+# The `<head>` tag Editor
+
+This is head tag editor.
+
+The installation process is very similar to [this repository][ads.txt_repo]
+
+Download this repo as .zip and upload on Plugin Managiment Console; We can use download .zip as it is.
+
+Go to Setting > Header Editor
+
+Add any necessary code.
+
+
+
+[ads.txt_repo]: https://github.com/kwtki/wordpress-ads.txt-editor
